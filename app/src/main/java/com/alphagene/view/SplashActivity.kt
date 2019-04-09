e171143534e3d30bd2e7f9d857a2ec808c75f836
Ahmed Abdel Fatah <ahmedafatah@outlook.com>
2019-04-09 21:42:55 +0200

    splash screen

---
+++ app/src/main/java/com/alphagene/view/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.alphagene.view
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.alphagene.MainActivity
+import android.os.Handler
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val handler = Handler()
+        handler.postDelayed(Runnable { finish() }, 1000)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
