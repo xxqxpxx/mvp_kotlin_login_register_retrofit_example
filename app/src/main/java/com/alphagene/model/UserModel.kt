df4e137fbfd7fc474fdd269a1040dc431078b058
Ahmed Abdel Fatah <ahmedafatah@outlook.com>
2019-04-12 15:48:33 +0200

    PR fix

--- app/src/main/java/com/alphagene/model/UserModel.kt
+++ app/src/main/java/com/alphagene/model/UserModel.kt
@@ -5,6 +5,6 @@ class UserModel(name: String, passwd: String) : IUser {
     override var passwd: String = passwd
 
     override fun checkUserValidity(name: String, passwd: String): Boolean {
-        return !(this.name != name || this.passwd != passwd)
+        return (this.name == name && this.passwd == passwd)
     }
 }
