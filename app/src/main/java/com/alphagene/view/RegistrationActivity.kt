cc08efd5625df7c78e9649543928c80bd56f9d18
Ahmed Abdel Fatah <ahmedafatah@outlook.com>
2019-04-12 15:29:48 +0200

    UI Design

---
+++ app/src/main/java/com/alphagene/view/RegistrationActivity.kt
@@ -0,0 +1,13 @@
+package com.alphagene.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.alphagene.R
+
+class RegistrationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
